3f70997d845734591f37499c99c0a54cab024219
loricurreli <104564805+loricurreli@users.noreply.github.com>
2022-06-17 12:08:30 +0200

    Es 47

---
+++ Esercizio 47/exercise-4.js
@@ -0,0 +1,14 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+
+const {id, firstName, lastName, age} = person ;
+console.log(
+`id: ${id}
+firstName: ${firstName}
+lastName: ${lastName}
+age: ${age}`);
